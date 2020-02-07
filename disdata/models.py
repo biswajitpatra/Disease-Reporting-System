9d65c43f190a5a3c98b4381f353b111106f7e9c2
Shibani Shankar Dash <shibanidash71@gmail.com>
2020-02-07 14:18:07 +0530

    Fix Report Model

--- disdata/models.py
+++ disdata/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
 
@@ -9,11 +10,15 @@ class Disease(models.Model):
     morbidity = models.FloatField()
     info_diagnostic = models.TextField()
     info_managerial = models.TextField()
+    def __str__(self):
+        return self.disease_name
 
 class Report(models.Model):
     source = models.ForeignKey(User, on_delete=models.CASCADE)
     disease = models.ForeignKey(Disease, on_delete=models.CASCADE)
-    reported_on = models.DateTimeField()
+    reported_on = models.DateTimeField(default=timezone.now)
     mortality = models.FloatField()
     morbidity = models.FloatField()
     infections = models.IntegerField()
+    def __str__(self):
+        return "{} report from {} reported at {}".format(self.disease, self.source, self.reported_on)
