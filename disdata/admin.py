7e990829d026676fcbf92087b1232c4c5ac8f5cc
Shibani Shankar Dash <shibanidash71@gmail.com>
2020-02-07 14:17:48 +0530

    Restrict Forms for staff

--- disdata/admin.py
+++ disdata/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
 
 # Register your models here.
 
-from .models import Disease
+from .models import Disease, Report
 
-admin.register(Disease)
+admin.site.register(Disease)
+
+class ReportAdmin(admin.ModelAdmin):
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "source" and not request.user.is_superuser:
+            kwargs["queryset"] = User.objects.filter(id=request.user.id)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+admin.site.register(Report, ReportAdmin)
