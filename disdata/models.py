ae90d27f57ba746568f727f942ea19193029477a
Biswajit <patrabiswajit133@gmail.com>
2020-06-13 13:50:15 +0530

    added messaging

--- disdata/models.py
+++ disdata/models.py
@@ -2,8 +2,19 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
+import requests
+import json
 # Create your models here.
-
+def sendPostRequest(reqUrl, apiKey, secretKey, useType, phoneNo, senderId, textMessage):
+  req_params = {
+  'apikey':apiKey,
+  'secret':secretKey,
+  'usetype':useType,
+  'phone': phoneNo,
+  'message':textMessage,
+  'senderid':senderId
+  }
+  return requests.post(reqUrl, req_params)
 class Disease(models.Model):
     disease_name = models.CharField(max_length=1024)
     mortality = models.FloatField()
@@ -23,9 +34,11 @@ class Report(models.Model):
     def __str__(self):
         return "{} report from {} reported at {}".format(self.disease, self.source, self.reported_on)
     def save(self, *args, **kwargs):
-        if not self.pk:
-            source_user = User.objects.get(id=self.source.pk)
-            risk_population = Person.objects.filter(city=source_user.hospital.city)
+        # if not self.pk:
+        source_user = User.objects.get(id=self.source.pk)
+        risk_population = Person.objects.filter(city=source_user.hospital.city)
+        print("done",risk_population)
+        if self.mortality>10:
             for person in risk_population:
                 person.notify()
         super(Report, self).save(*args, **kwargs)
@@ -39,8 +52,10 @@ class Person(models.Model):
     def __str__(self):
         return "{} from {}".format(self.full_name, self.city)
     def notify(self):
-        # Use this function to notify user
-        pass
+        URL = 'https://www.sms4india.com/api/v1/sendCampaign'
+        response = sendPostRequest(URL, "PT5VPWDIY5UHKR5W9Z7LJA723XAJ4O9L", 'VOT5R4ICAHKB80QP', 'stage', '+919439832766', 'sai.nayak1503@gmail.com', 'Swine flu detected in your vicinity' )
+        print(response.text)
+
 
 class Hospital(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
