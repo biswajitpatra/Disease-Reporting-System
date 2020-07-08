b2d17c7811f5a03acccc84be36198cbd35e97217
Biswajit <patrabiswajit133@gmail.com>
2020-07-09 01:44:27 +0530

    Added pincode functionality

--- disdata/models.py
+++ disdata/models.py
@@ -1,6 +1,7 @@
 from django.contrib.gis.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from django.core.validators import MinLengthValidator
 
 import requests
 import json
@@ -15,12 +16,39 @@ def sendPostRequest(reqUrl, apiKey, secretKey, useType, phoneNo, senderId, textM
   'senderid':senderId
   }
   return requests.post(reqUrl, req_params)
+
+class Pincode(models.Model):
+    pincode=models.CharField(max_length=6,validators=[MinLengthValidator(6)],primary_key=True,unique=True)
+    area=models.CharField(max_length=40)
+    state=models.CharField(max_length=40)
+    state_code=models.IntegerField()
+    province=models.CharField(max_length=40)
+    province2=models.CharField(max_length=40)
+    accuracy=models.IntegerField()
+    located_at=models.PointField()
+
+    def __str__(self):
+        return self.pincode
+
+class Hospital(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=40)
+    city = models.CharField(max_length=32)
+    pincode= models.ForeignKey(Pincode,on_delete=models.CASCADE)
+    located_at = models.PointField()
+    def __str__(self):
+        return self.name
+
 class Disease(models.Model):
     disease_name = models.CharField(max_length=1024)
+    zoonotic = models.BooleanField(default=False)
     mortality = models.FloatField()
     morbidity = models.FloatField()
+    vacination_available=models.BooleanField(default=False)
+    info_spread=models.TextField()
     info_diagnostic = models.TextField()
     info_managerial = models.TextField()
+    info_url = models.URLField(max_length=200)
     def __str__(self):
         return self.disease_name
 
@@ -28,10 +56,12 @@ class Report(models.Model):
     source = models.ForeignKey(User, on_delete=models.CASCADE)
     disease = models.ForeignKey(Disease, on_delete=models.CASCADE)
     reported_on = models.DateTimeField(default=timezone.now)
-    reported_at = models.PointField()
     mortality = models.FloatField()
     morbidity = models.FloatField()
     infections = models.IntegerField()
+    death = models.BooleanField()
+    pincode = models.ForeignKey(Pincode,on_delete=models.CASCADE)
+    reported_at = models.PointField()
     def __str__(self):
         return "{} report from {} reported at {}".format(self.disease, self.source, self.reported_on)
     def save(self, *args, **kwargs):
@@ -45,11 +75,14 @@ class Report(models.Model):
         super(Report, self).save(*args, **kwargs)
 
 class Person(models.Model):
-    full_name = models.CharField(max_length=1024)
+    full_name = models.CharField(max_length=24)
     email = models.EmailField()
-    gender = models.CharField(max_length=6)
-    phone_number = models.CharField(max_length=20)
+    aadhar_number=models.CharField(unique=True,primary_key=True,max_length=12,validators=[MinLengthValidator(12)])
+    gender = models.CharField(choices = (("Male","Male"),("Female","Female")),max_length=6)
+    phone_number = models.CharField(max_length=10)
     city = models.CharField(max_length=32)
+    pincode = models.ForeignKey(Pincode,on_delete=models.CASCADE)
+    animal_owner = models.BooleanField()
     located_at = models.PointField()
     def __str__(self):
         return "{} from {}".format(self.full_name, self.city)
@@ -58,8 +91,3 @@ class Person(models.Model):
         response = sendPostRequest(URL, "PT5VPWDIY5UHKR5W9Z7LJA723XAJ4O9L", 'VOT5R4ICAHKB80QP', 'stage', '+919439832766', 'sai.nayak1503@gmail.com', 'Swine flu detected in your vicinity' )
         print(response.text)
 
-
-class Hospital(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    city = models.CharField(max_length=32)
-    located_at = models.PointField()
