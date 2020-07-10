7f490381d174b5f29d711bd47098aa6b3283dd97
saiprasadnayak6858 <saiprasadnayak6858@gmail.com>
2020-07-10 15:04:16 +0530

    integrated geocoding API

---
+++ disdata/static/js/geolocation.js
@@ -0,0 +1,25 @@
+$(document).ready(() => {
+    
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(showPosition);
+    }
+    else{
+        $('#user-location').html('Not supporting')
+    }
+
+    function showPosition(position){
+
+        // $('#user-location').html(`Latitude: ${position.coords.latitude} , Longitude: ${position.coords.longitude}`)
+
+        let locationAPI = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${position.coords.latitude},${position.coords.longitude}&key=AIzaSyDI0rZJabSHwnaVtPU71KsqokaqowHSQ70` 
+        // let locationAPI = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&key=AIzaSyDI0rZJabSHwnaVtPU71KsqokaqowHSQ70'
+
+        $.get({
+            url: locationAPI,
+            success: function(data) {
+                console.log(data);
+                $('#user-location').html(data.results[5].formatted_address);
+            }
+        })
+    }
+})
