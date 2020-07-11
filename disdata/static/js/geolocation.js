f8e0052987b424945c679343a29bc98ace0ad716
saiprasadnayak6858 <saiprasadnayak6858@gmail.com>
2020-07-12 00:46:10 +0530

    optimized location tracking

--- disdata/static/js/geolocation.js
+++ disdata/static/js/geolocation.js
@@ -1,12 +1,18 @@
 
    const knowMyLocation = () => {
+     
+       options = {
+           enableHighAccuracy:true
+       }
     if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(showPosition);
+        navigator.geolocation.watchPosition(showPosition, error, options);
     }
     else{
-        $('#user-location').html('Not supporting')
+        $('#user-location').html('Browser does not support geolocation')
     }
-
+    function error(err) {
+        console.warn('ERROR(' + err.code + '): ' + err.message);
+      }
     function showPosition(position){
 
         // $('#user-location').html(`Latitude: ${position.coords.latitude} , Longitude: ${position.coords.longitude}`)
@@ -19,7 +25,9 @@
             success: function(data) {
                 console.log(data);
                 const length = data.results.length;
-                $('#user-location').html(`You are currently at :${data.results[length-3].formatted_address}`);
+                // $('#location-search').attr('placeholder',`${data.results[length-3].formatted_address}`)
+                $('#location-search').attr('placeholder',`${data.plus_code.compound_code.slice(7)}`)
+                // $('#user-location').html(`You are currently at :${data.results[length-3].formatted_address}`);
             }
         })
     }
