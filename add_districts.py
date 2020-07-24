bc7ea5ceeb198b8247e91a55ca08a2f9fb6f98fe
Biswajit <patrabiswajit133@gmail.com>
2020-07-24 19:05:05 +0530

    Resolved error

--- add_districts.py
+++ add_districts.py
@@ -15,7 +15,7 @@ for p in Pincode.objects.all():
     if p.state_code in states: 
         if(District.objects.filter(name=p.province).exists()==False):
             u = User.objects.create_user(username=p.province+"@123",email=p.province+"@gov.com",password="gocoronago")
-            d = District.objects.create(name=p.province,district_official=u,rainfall=0.0,altitude=20,temprature=0,population=0,water_source=0,humidity=0.0,age_frequency_vector=[0,0,0],slums_count=0,wind=0,victim_ids=['pt'])
+            d = District.objects.create(name=p.province,district_official=u,rainfall=0.0,altitude=20,temprature=0,population=0,water_source=0,humidity=0.0,age_frequency_vector=[0,0,0],density=0,slums_count=0,wind=0,victim_ids=['pt'])
             p.district = d
             u.is_staff =True
             u.save()
